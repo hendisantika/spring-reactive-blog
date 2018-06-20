fe116c42a11244d8f1a0db9d62171691d694a7db
Hendi Santika <hendisantika@gmail.com>
2018-06-20 11:39:07 +0700

    #3 - Add User Domain

---
+++ src/main/kotlin/com/hendisantika/springreactiveblog/domain/User.java
@@ -0,0 +1,68 @@
+package com.hendisantika.springreactiveblog.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-reactive-blog
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/06/18
+ * Time: 11.38
+ * To change this template use File | Settings | File Templates.
+ */
+@Document
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        return name != null ? name.equals(user.name) : user.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
