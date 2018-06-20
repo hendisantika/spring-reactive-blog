be2f52ddccedd452537a6233c1b144515e0125b8
Hendi Santika <hendisantika@gmail.com>
2018-06-20 11:34:29 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/springreactiveblog/SpringReactiveBlogApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.springreactiveblog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringReactiveBlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringReactiveBlogApplication>(*args)
+}
